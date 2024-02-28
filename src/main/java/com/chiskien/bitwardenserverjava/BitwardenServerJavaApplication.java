fe2f1d0345247e7d7bdc7b4f38d2cdbd091356a6
ChisKien <chiskien03214@gmail.com>
2024-02-28 10:38:20 +0700

    demo grafana with loki

---
+++ src/main/java/com/chiskien/bitwardenserverjava/BitwardenServerJavaApplication.java
@@ -0,0 +1,21 @@
+package com.chiskien.bitwardenserverjava;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@Slf4j
+public class BitwardenServerJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitwardenServerJavaApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner applicationRunner() {
+        return args -> log.info("Hello Bitwarden Server");
+    }
+}
---
+++ src/test/java/com/chiskien/bitwardenserverjava/BitwardenServerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiskien.bitwardenserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitwardenServerJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
