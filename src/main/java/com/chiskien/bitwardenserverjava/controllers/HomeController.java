6ae990c7bfb67fa64fda8100432726db14249904
ChisKien <chiskien03214@gmail.com>
2024-03-01 10:12:32 +0700

    Testing distributed tracing with OpenTelemetry and Tempo (Grafana stack)

--- src/main/java/com/chiskien/bitwardenserverjava/controllers/HomeController.java
+++ src/main/java/com/chiskien/bitwardenserverjava/controllers/HomeController.java
@@ -1,15 +1,18 @@
 package com.chiskien.bitwardenserverjava.controllers;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HomeController {
 
     @GetMapping("/home")
     public ResponseEntity<String> hello() {
+        log.info("Incoming Request");
         return ResponseEntity.ok("Hello Worlds");
     }
 }
